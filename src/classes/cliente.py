d0b931beed5edb9cc5ba3eac5a3427dfee6a73a3
luisf <luisfelipehcarmona@gmail.com>
2024-11-27 08:02:54 -0300

    base classe cliente

--- src/classes/cliente.py
+++ src/classes/cliente.py
@@ -1,3 +1,6 @@
+from linhatelefonica import Linha
+from usuario import Usuario
 class Cliente:
-    def __init__(self, x, y):
-        pass
+    def __init__(self, data_usuario : list | tuple, data_linha : list | tuple):
+        self.linha = Linha(data_linha)
+        self.usuario = Usuario(data_usuario)
