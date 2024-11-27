5c1ac14d27a10cbe72282767ea95338bada77bb3
luisf <luisfelipehcarmona@gmail.com>
2024-11-27 10:26:43 -0300

    documentação classe Linha

--- src/classes/linhatelefonica.py
+++ src/classes/linhatelefonica.py
@@ -1,7 +1,13 @@
 class Linha:
     def __init__(self, dados):
+        """
+        Inicializa um objeto da classe Linha com os dados fornecidos.
+        """
         telefone, plano = dados
         self.telefone = telefone
         self.plano = plano
     def obter_dados(self):
+        """
+        Retorna os dados de um objeto Linha dentro de uma tupla
+        """
         return self.telefone, self.plano
