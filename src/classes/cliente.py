d3264d287a209cede0b120620c367c56ff5ede1a
luisf <luisfelipehcarmona@gmail.com>
2024-11-27 10:26:00 -0300

    documentação e metodo obter_dados na classe clientes

--- src/classes/cliente.py
+++ src/classes/cliente.py
@@ -1,6 +1,14 @@
-from linhatelefonica import Linha
-from usuario import Usuario
+from classes.linhatelefonica import Linha
+from classes.usuario import Usuario
 class Cliente:
     def __init__(self, data_usuario : list | tuple, data_linha : list | tuple):
+        """
+        Inicializa um objeto da classe Usuario e um Linha com os dados fornecidos.
+        """
         self.linha = Linha(data_linha)
         self.usuario = Usuario(data_usuario)
+    def obter_dados(self):
+        """
+        Retorna os dados de um Cliente dentro de uma tupla
+        """
+        return self.usuario.obter_dados() + self.linha.obter_dados()
