314a1d80dcf22ee072c8e26f87bc4750f09e1d03
luisf <luisfelipehcarmona@gmail.com>
2024-11-27 09:29:06 -0300

    estrutura  usuario e metodo retorno de dados

--- src/classes/usuario.py
+++ src/classes/usuario.py
@@ -1,3 +1,15 @@
 class Usuario:
-    def __init__(self, x, y):
-        pass
+    def __init__(self, dados):
+        """
+        Inicializa um objeto da classe Usuario com os dados fornecidos.
+        """
+        nome, email, endereco = dados
+        self.nome = nome
+        self.email = email
+        self.endereco = endereco
+    def obter_dados(self):
+        """
+        Retorna os dados de um Usuario dentro de uma tupla
+        """
+        return self.nome, self.email, self.endereco
+    
